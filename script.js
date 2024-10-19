9fd0ed474fd9ff4ce73800367931a6435a4a2be4
biba9 <sebashamsaldeen@gmail.com>
2024-10-19 05:49:32 +0300

    task5-2

--- script.js
+++ script.js
@@ -5,24 +5,40 @@ let toUnit = document.querySelector('#toUnit');
 
 function convert() {
 
-    let resulte=0;
+    let resulte = 0;
     let value = parseFloat(document.getElementById('inputValue').value);
-
-    if (fromUnit.value === 'meter') {
+    if (
+        (fromUnit.value == 'kilometer' && toUnit.value == 'kilometer')
+        || (fromUnit.value == 'meter' && toUnit.value == 'meter')
+        || (fromUnit.value == 'centimeter' && toUnit.value == 'centimeter')) {
         resulte = Number(value);
-    } else if (fromUnit.value === 'kilometer') {
-        resulte = Number(value )* 1000; // كيلومتر إلى متر
-    } else if (fromUnit.value === 'centimeter') {
+
+    }
+    else if ((fromUnit.value == 'meter') && (toUnit.value == 'kilometer')) {
+        resulte = Number(value) / 1000;
+    }
+    else if ((fromUnit.value == 'kilometer') && (toUnit.value == 'meter')) {
+        resulte = Number(value) * 1000; // كيلومتر إلى متر
+    }
+    else if ((fromUnit.value == 'centimeter') && (toUnit.value == 'meter')) {
         resulte = Number(value) / 100; // سنتيمتر إلى متر
     }
-
-    if (toUnit === 'meter') {
-    } else if (toUnit.value === 'kilometer') {
-        resulte =Number(value) / 1000; // متر إلى كيلومتر
-    } else if (toUnit.valuet === 'centimeter') {
+    else if ((fromUnit.value == 'meter') && (toUnit.value == 'centimeter')) {
         resulte = Number(value) * 100; // متر إلى سنتيمتر
     }
+    else if ((fromUnit.value == 'meter') && (toUnit.value == 'kilometer')) {
+        resulte = Number(value) / 1000; // متر إلى كيلومتر
+    }
     
+    else if((fromUnit.value == 'meter') && (toUnit.value == 'centimeter')) {
+        resulte = Number(value) * 100; // متر إلى سنتيمتر
+    }
+    else {
+      document.getElementById('inputValue').value = "error"; // في حالة عدم وجود عملية
+
+    
+    }
+
     convertedOutput.value  = resulte;
 }
 convertUnit.addEventListener("click", convert);
